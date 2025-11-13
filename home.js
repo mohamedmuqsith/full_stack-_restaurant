b9c663a21963ef1208565989ea6e0106c0c5a89e
mohamedmuqsith <mohamedmukshidh35@gmail.com>
2025-11-13 21:56:37 +0530

    mukshith updated home page index.html

--- home.js
+++ home.js
@@ -1,6 +1,21 @@
- const menuIcon = document.querySelector(".menu-icon");
+    const menuIcon = document.querySelector(".menu-icon");
     const mobileMenu = document.querySelector(".mobile-menu");
 
     menuIcon.addEventListener("click", () => {
-        mobileMenu.classList.toggle("active");
+      mobileMenu.classList.toggle("active");
+
+      const icon = menuIcon.querySelector("i");
+      icon.classList.toggle("fa-bars");
+      icon.classList.toggle("fa-xmark");
+    });
+
+    // Close mobile menu when clicking on a link
+    const mobileLinks = document.querySelectorAll(".mobile-menu ul li");
+    mobileLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        mobileMenu.classList.remove("active");
+        const icon = menuIcon.querySelector("i");
+        icon.classList.add("fa-bars");
+        icon.classList.remove("fa-xmark");
+      });
     });
